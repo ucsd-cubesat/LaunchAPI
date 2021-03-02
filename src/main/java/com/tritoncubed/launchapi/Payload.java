e3daa4b6bee4dd5b5cb2c5b5db02fbb3a74bcaee
Christopher Madrigal <c4madrig@ucsd.edu>
2021-03-01 16:54:19 -0800

    fix formatting, rewrite backend

--- src/main/java/com/tritoncubed/launchapi/Payload.java
+++ src/main/java/com/tritoncubed/launchapi/Payload.java
@@ -1,33 +1,40 @@
 package com.tritoncubed.launchapi;
 
-/** 
- * A POJO containing the data for a payload made through an HTTP request. Payloads are serialized
- * to, and deserialized from, the Dynamo DB as an intermediate format. Both the front-facing HTTP
- * API and back-facing Amazon code should conform to the Payload format.
- * 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+/**
+ * A POJO containing the data for a payload made through an HTTP request.
+ * Payloads are serialized to, and deserialized from, the Dynamo DB as an intermediate format. Both
+ * the front-facing HTTP API and back-facing Amazon API should conform to the Payload format.
+ *
  * To better automate the connection to the back-end, fields must follow these guidelines:
- * 
- * - Compound types are not allowed, such as:
- *   - Arrays
- *   - Containers (List, Map, etc)
- *   - Other POJOs
- * - Numeric values must be of their object type; i.e. Integer, Double, etc
- * 
+ *   - Compound types (arrays, containers, other POJOs) are not allowed
+ *   - Numeric values must be of their object type (i.e. Integer, Double, etc)
+ *
+ *
  * Be sure to document the source of each field, including such information as:
  *   - The source of the data point
  *   - Units of measurement, where applicable
  *   - A description of the field
  */
+@DynamoDBTable(tableName = "LaunchDB")
 public class Payload {
 
-	/**
-	 * UTC time in seconds, from the GPS
-	 */
-	public Long utc;
-	
-	/**
-	 * TODO
-	 */
-	public String temp;
+    /**
+     * UTC time in seconds, from the GPS
+     */
+    private Long utc;
+    @DynamoDBHashKey
+    public Long getUtc() { return utc; }
+    public void setUtc(Long utc) { this.utc = utc; }
 
+    /**
+     * TODO
+     */
+    private String temp;
+    @DynamoDBAttribute
+    public String getTemp() { return temp; }
+    public void setTemp(String temp) { this.temp = temp; }
 }
--- src/main/java/com/tritoncubed/launchapi/PingServlet.java
+++ src/main/java/com/tritoncubed/launchapi/PingServlet.java
@@ -1,6 +1,8 @@
 package com.tritoncubed.launchapi;
 
 import java.io.IOException;
+import javax.json.Json;
+import javax.json.JsonObject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,21 +15,22 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/ping")
 public class PingServlet extends HttpServlet {
-       
-	private static final long serialVersionUID = 6659111915305916881L;
 
-	public PingServlet() {
+    private static final long serialVersionUID = 6659111915305916881L;
+
+    public PingServlet() {
         super();
     }
 
-	/**
-	 * This method contains the logic of the ping request. The request itself is contained in its
-	 * namesake object, and the response is created in its namesake object.
-	 */
+    /**
+     * This method contains the logic of the ping request. The request itself is contained in its
+     * namesake object, and the response is created in its namesake object.
+     */
     @Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		response.getWriter().append("Pong!\n");
-	}
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        JsonObject json = Json.createObjectBuilder()
+                .add("response", "pong")
+                .build();
+        response.getWriter().append(json.toString());
+    }
 }
