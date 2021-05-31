f4129ca4a0c8edf8d23f865a74a6ad644a59500d
Roy Sanchez <roysanchez@Roys-MacBook-Pro.local>
2021-05-31 10:20:31 -0700

    Added GPS coordinates to payload

--- src/main/java/com/tritoncubed/launchapi/DBServlet.java
+++ src/main/java/com/tritoncubed/launchapi/DBServlet.java
@@ -3,6 +3,8 @@ package com.tritoncubed.launchapi;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.util.Arrays;
+import java.util.Calendar;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -34,12 +36,12 @@ public class DBServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	byte[] raw = request.getInputStream().readAllBytes(); 
 		ByteBuffer buffer = ByteBuffer.wrap(raw).order(ByteOrder.LITTLE_ENDIAN);
-		
+				
 		System.out.println("Buffer's capacity: " + buffer.capacity());
 		
-		if(buffer.capacity() != 22) { 
+		if(buffer.capacity() != 48) { 
 			response.setStatus(400); 
-			response.getWriter().append("{\"error\" : \"Payload is not 22 bytes. Got " + buffer.capacity() + "\" }" ); 
+			response.getWriter().append("{\"error\" : \"Payload is not 48 bytes. Got " + buffer.capacity() + "\" }" ); 
 			return;
 		} 
 		// Normalize values and insert into payload 
@@ -54,11 +56,47 @@ public class DBServlet extends HttpServlet {
 		short mag_Z = buffer.getShort(); // 2 bytes
 		float temp_C = buffer.getFloat(); // 4 bytes
 		
-		Payload payload = new Payload(accel_X, accel_Y, accel_Z, gyro_X, gyro_Y, gyro_Z, mag_X, mag_Y, mag_Z, temp_C);
+		byte[] lat = new byte[12];
+		byte[] lon = new byte[12];
+		buffer.get(lat);
+		String latitude = stringFromBytes(lat);
+		char NS = (char) buffer.get(); // N S as 1 byte (N +, S -)
+		buffer.get(lon);
+		String longitude = stringFromBytes(lon); 
+		char EW = (char) buffer.get(); //E W as 1 byte (E +, W -)
+		// convert to float
+		if(latitude.isEmpty()) {
+			response.setStatus(400);
+			//TODO throw JSON error
+			return;
+		}
+		
+		float lat_float = Integer.valueOf(latitude.substring(0,2)) + Float.valueOf(latitude.substring(2));
+		float lon_float = Integer.valueOf(longitude.substring(0,3)) + Float.valueOf(longitude.substring(3));
+		
+		if(NS == 'S') {
+			lat_float *= -1;
+		}
+		if(EW == 'W') {
+			lon_float *= -1;
+		}
+	
+		long utc = Calendar.getInstance().getTimeInMillis();
+		
+		Payload payload = new Payload(accel_X, accel_Y, accel_Z, gyro_X, gyro_Y, gyro_Z, mag_X, mag_Y, mag_Z, temp_C, lat_float, lon_float, utc);
 		
 		System.out.println(payload.toString());
 	
 		response.getWriter().append(payload.toString());
 		//LaunchDB.put(payload); // inserting payload into DB
     }
+    
+    private static String stringFromBytes(byte[] bytes) {
+        int idxOfNull;
+        for (idxOfNull = 0; idxOfNull < bytes.length && bytes[idxOfNull] != 0; idxOfNull++);
+        bytes = Arrays.copyOfRange(bytes, 0, idxOfNull);
+        return new String(bytes);
+    }
+    
+    
 }
